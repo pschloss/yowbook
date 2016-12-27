b741034ccf49cdecffe2e97aa11ae9196004137f
Pat Schloss <pschloss@umich.edu>
2016-12-27 13:39:12 -0500

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
   get '/news',      to: 'static_pages#news'
 
 	get '/signup',    to: 'users#new'
-	post '/signup',   to: 'users#create'
 
 	get '/login',     to: 'sessions#new'
 	post '/login',    to: 'sessions#create'
 	delete '/logout', to: 'sessions#destroy'
 
 	resources :users
+	resources :account_activations, only: [:edit]
 end
