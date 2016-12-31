d9074b9315d7b68f5731923d5682febc2880f759
Pat Schloss <pschloss@umich.edu>
2016-12-31 15:46:53 -0500

    Put users's flock on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,8 @@ class StaticPagesController < ApplicationController
 
   def home
 		if logged_in?
+			@animals = current_shepherd.animals.paginate(page: params[:page])
 			@animal = current_shepherd.animals.build if logged_in?
-			@feed_items = current_shepherd.feed.paginate(page: params[:page])
 		end
   end
 
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -6,6 +6,8 @@ class Animal < ApplicationRecord
 	validates :eartag, presence: true, length: { maximum: 20 }
 	validates_date :birth_date, presence: true, :on_or_before => lambda { Date.current }
 	validate :picture_size
+	before_save :refresh_shepherd
+
 
 	private
 
@@ -13,6 +15,11 @@ class Animal < ApplicationRecord
 			if picture.size > 5.megabytes
 				errors.add(:picture, "should be less than 5MB")
 			end
+		end
+
+		#touch the shepherd's record to update the updated_at field
+		def refresh_shepherd
+		  Shepherd.find_by(id: shepherd_id).touch
 		end
 
 end
